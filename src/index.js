7824b91daf80322b5b79d939a773316c985241bd
Asma Kolaib <as.kolaib@gmail.com>
2020-08-13 04:18:28 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+  let submit = document.getElementById('create-task-form');
+  submit.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let input = document.getElementById("new-task-description").value;
+     let ul = document.getElementById("#tasks");
+     let li = document.createElement('li');
+     li.innerHTML =`${input} `;
+     list.appendChild(li);
+     console.log(ul);
+
+});
+
 });
